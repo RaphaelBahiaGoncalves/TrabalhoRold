9499a157e185602131b79e1a8431af061216ca80
RaphaelBahiaGoncalves <raphaelbahia11@gmail.com>
2025-10-31 20:43:29 -0300

    Add files via upload
    Este commit é o que o profeessor pediu para fazer

---
+++ conectar.php
@@ -0,0 +1,8 @@
+<?php
+/* Dados do Banco de Dados a conectar */
+$Servidor = 'localhost';
+$nomeBanco = 'banco_teste';
+$Usuario = 'fabio';
+$Senha = '123';
+$strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco);
+?>
